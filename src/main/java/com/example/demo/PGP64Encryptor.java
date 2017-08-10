ebf5e9ce5e8bc002c76da58e3aaab34eb4f5dcd8
Arun Shankar Tiwari <arunstiwari@gmail.com>
2017-08-10 11:54:16 +0530

    Modified the file

--- src/main/java/com/example/demo/PGP64Encryptor.java
+++ src/main/java/com/example/demo/PGP64Encryptor.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class PGP64Encryptor implements Encrypt {
 
 	public String encrypt(String status) {
+		System.out.println("PGP64 Encryptiion");
 		return "PGP64 Encryption";
 	}
 
